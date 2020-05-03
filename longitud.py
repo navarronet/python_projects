8eded0429eff20181d126a531dc71db3cf8290dd
Milton Navarro <milton.j.navarro.p@gmail.com>
2020-05-03 12:24:19 -0600

    first remote commit

--- longitud.py
+++ longitud.py
@@ -1,6 +1,6 @@
 PI = 3.1416 #PI number
 
-radio = input("Please, enter the radius of the circumference: ")
+radio = input("Please, enter the radius of the circumference: ") #ask the user to enter some value
 
 longitud = 2 * PI * float(radio)
 
