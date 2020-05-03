0f6a34b29a26728c91dde3cb04273c00e3238118
Milton Navarro <milton.j.navarro.p@gmail.com>
2020-05-03 12:28:30 -0600

    final commit

--- longitud.py
+++ longitud.py
@@ -2,6 +2,6 @@ PI = 3.1416 #PI number
 
 radio = input("Please, enter the radius of the circumference: ") #ask the user to enter some value
 
-longitud = 2 * PI * float(radio)
+longitud = 2 * PI * float(radio) #calculation
 
 print("The length of the circumference is: ", round(longitud,2))
